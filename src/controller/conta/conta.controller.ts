5bf775de71a099e6f767bf25a3df5f2c50154b2b
VitorCostaTI <vitortecnologia62@gmail.com>
2024-06-25 13:25:46 -0300

    Conta: adicionar resposta statusCode

--- src/controller/conta/conta.controller.ts
+++ src/controller/conta/conta.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Post, Put } from '@nestjs/common';
 import { Conta } from 'src/Model/Contas';
 import { ContaService } from 'src/services/conta/conta.service';
 import { FindOneOptions } from 'typeorm';
@@ -8,31 +8,37 @@ export class ContaController {
     constructor(private contaService: ContaService) { }
 
     @Post()
+    @HttpCode(201)
     async create(@Body() conta: Conta) {
         return await this.contaService.create(conta);
     }
-
+    
     @Get()
+    @HttpCode(200)
     async get() {
         return await this.contaService.findAll()
     }
-
+    
     @Get(':id')
+    @HttpCode(200)
     async getId(@Param('id') id: number) {
         const options: FindOneOptions<Conta> = {
             where: {id}
         }
-
+        
         return await this.contaService.findOne(id)
     }
-
+    
     @Put(':id')
+    @HttpCode(201)
     async update(@Param('id') id: number, @Body() conta: Conta) {
         return await this.contaService.update(id, conta);
     }
-
+    
     @Delete(':id')
+    @HttpCode(201)
     async delete(@Param('id') id: number){
         return await this.contaService.delete(id)
     }
 }
+
