ca1eb284feed539a43e6d4a11533e276fcaf53f7
VitorCostaTI <vitortecnologia62@gmail.com>
2024-06-25 13:10:06 -0300

    Auditoria: Gera model Auditoria

---
+++ src/Model/Auditoria.ts
@@ -0,0 +1,16 @@
+import { IsNotEmpty } from "class-validator";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Auditoria{
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    @IsNotEmpty()
+    descricao: string;
+
+    @Column()
+    @IsNotEmpty()
+    data: Date
+}
