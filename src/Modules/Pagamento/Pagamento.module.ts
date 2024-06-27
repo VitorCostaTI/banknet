fd2dc1e7e4201cc553b9312e37ac48717394ef54
VitorCostaTI <vitortecnologia62@gmail.com>
2024-06-27 07:43:13 -0300

    Pagamentos: implementa logica de atualização de saldo no cadastro

--- src/Modules/Pagamento/Pagamento.module.ts
+++ src/Modules/Pagamento/Pagamento.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Conta } from 'src/Model/Contas';
 import { Pagamentos } from 'src/Model/Pagamentos';
 import { PagamentoController } from 'src/controller/pagamento/pagamento.controller';
 import { PagamentosService } from 'src/services/pagamentos/pagamentos.service';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Pagamentos])],
+    imports: [TypeOrmModule.forFeature([Pagamentos, Conta])],
     providers: [PagamentosService],
     controllers: [PagamentoController]
 })
--- src/services/pagamentos/pagamentos.service.ts
+++ src/services/pagamentos/pagamentos.service.ts
@@ -1,13 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Conta } from 'src/Model/Contas';
 import { Pagamentos } from 'src/Model/Pagamentos';
 import { FindOneOptions, Repository } from 'typeorm';
 
 @Injectable()
 export class PagamentosService {
     constructor(
+        @InjectRepository(Conta)
+        private contaRepository: Repository<Conta>,
         @InjectRepository(Pagamentos)
-        private pagamentoRepository: Repository<Pagamentos>
+        private pagamentoRepository: Repository<Pagamentos>,
     ) { }
 
     async findAll(): Promise<Pagamentos[]> {
@@ -21,8 +24,29 @@ export class PagamentosService {
         return await this.pagamentoRepository.findOne(options);
     }
 
-    async create(pagamento: Pagamentos) {
-        return await this.pagamentoRepository.save(pagamento)
+    async create(pagamento: Partial<Pagamentos>): Promise<Pagamentos> {
+
+        const optionsConta: FindOneOptions<Conta> = {
+            where: { id: pagamento.id_conta }
+        };
+
+        const conta = await this.contaRepository.findOne(optionsConta);
+        if (!conta) {
+            throw new NotFoundException(`Conta de ${ conta.nome} não encontrada`);
+        }
+
+        const novoPagamento = this.pagamentoRepository.create(pagamento);
+        const pagamentoSalvo = await this.pagamentoRepository.save(novoPagamento);
+
+        if(pagamento.valor > conta.saldo_inicial){
+            throw new NotFoundException(`Conta de ${ conta.nome} não possui saldo suficiente`);
+        }
+
+        // Atualizar o saldo da conta
+        conta.saldo_inicial = conta.saldo_inicial - pagamento.valor;
+        await this.contaRepository.save(conta);
+
+        return pagamentoSalvo;
     }
 
     async update(id: number, pagamento: Pagamentos): Promise<Pagamentos> {
@@ -33,7 +57,9 @@ export class PagamentosService {
 
         return await this.pagamentoRepository.findOne(options);
     }
+
     async delete(id: number): Promise<{ msg: String }> {
+        
         await this.pagamentoRepository.delete(id);
 
         return { msg: 'Pagamento deletado com sucesso' }
